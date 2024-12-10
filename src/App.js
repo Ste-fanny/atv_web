589ebebd2534e6bd81b93d372fccf218b13e0d22
Stefany Thais dos Santos Silva <2022102020046@aluno.ifro.edu.br>
2024-12-10 17:09:05 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Oii fessor </h1>
+        <p>Aluna: Stefany Thais a melhor aluna :)</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
